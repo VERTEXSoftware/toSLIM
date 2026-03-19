e038510657f7504e19047ac25bd32315bf0e51e1
VERTEXSoftware <vertexsoftwork@gmail.com>
2026-03-19 21:36:53 +0900

    fix color converted

--- include/SLIM/SLIM.h
+++ include/SLIM/SLIM.h
@@ -929,7 +929,7 @@ SLIMERROR SLIM_Read_Layer(SLIM_STREAM* file, SLIMLayerDesc* desc){
 					switch (m_CODE)
 					{
 					case SLIMCODE::CODE_GRAY:
-						m_IMG[index]		= (77 * chn0 + 150 * chn1 + 29 * chn2) >> 8;
+						m_IMG[index]		= (chn0 + chn1 + chn2) / (_slim_lh._channel == 4 ? 3 : _slim_lh._channel);
 						break;
 					case SLIMCODE::CODE_RGB:
 						m_IMG[index]		= chn0;
